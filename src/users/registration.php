f5b28edf66ccd11a13a83ec70e3f7436a2196dc0
matee8 <molnarmatenorbert@gmail.com>
2024-05-06 09:19:10 +0200

    Registration done

--- src/users/registration.php
+++ src/users/registration.php
@@ -5,40 +5,42 @@ if (isset($_POST["register"])) {
     $password = trim($_POST["password"]);
     $password_again = trim($_POST["password_again"]);
     $file = file_get_contents("../resources/data/users.txt");
+    $lines = explode("\n", $file);
+    $valid_registration = true;
     $i = 0;
 
     if ($password !== $password_again) {
-        echo("
-            <script>
-                alert(\"Sikeres bejelentkezés! Pár másodperc és átirányítunk.\");
-            </script>
-        ");
+        $valid_registration = false;
     }
 
-    $lines = explode("\n", $file);
-
-    for ($i = 0; $i < count($lines); $i++) {
-        $line = explode(";", $lines[$i]);
-        if (count($line) > 1) {
-            if ($line[0] == $username || $line[1] == $email 
-                || $line[2] == $password) {
-                echo("
-                    <script>
-                    alert(\"Sikeres bejelentkezés! Pár másodperc és átirányítunk.\");
-                    </script>
-                ");
+    if ($valid_registration) {
+        for ($i = 0; $i < count($lines); $i++) {
+            $line = explode(";", $lines[$i]);
+            if (count($line) > 1) {
+                if ($line[0] == $username || $line[1] == $email) {
+                    $valid_registration = false;
+                    break;
+                }
             }
         }
     }
-
-    file_put_contents("../resources/data/users.txt", 
-        $file . "\n" . $username . ";" . $email . ";" . $password);
-    header("refresh: 3; url=index.php?page=login");
-    echo("
-        <script>
-            alert(\"Sikeres regisztráció! Pár másodperc és átirányítunk\");
-        </script>
-    ");
+    
+    if ($valid_registration) {
+        file_put_contents("../resources/data/users.txt", 
+            $file . "\n" . $username . ";" . $email . ";" . $password);
+        header("refresh: 3; url=index.php?page=login");
+        echo("
+            <script>
+                alert(\"Sikeres regisztráció! Pár másodperc és átirányítunk\");
+            </script>
+        ");
+    } else {
+        echo("
+            <script>
+            alert(\"Sikertelen regisztráció!\");
+            </script>
+        ");
+    }
 }
 
 ?>
